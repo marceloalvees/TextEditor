7440ee215bde2f1acd93fcffd1bf260d7f3d440c
Marcelo Alves <marcelloalves09@gmail.com>
2024-01-02 19:20:12 -0300

    first commit

---
+++ TextEditor/Program.cs
@@ -0,0 +1,57 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("1- Open File");
+    Console.WriteLine("2- Create new file");
+    Console.WriteLine("0- Closed");
+    short option = short.Parse(Console.ReadLine());
+
+    switch(option)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Closed(); break;
+    }
+}
+
+static void Open()
+{
+
+}
+
+static void Closed()
+{
+    Console.Clear();
+    Console.WriteLine("Enter your text below: (ESC Closed) ");
+    Console.WriteLine("__________________");
+    string text = "";
+
+    do
+    {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+    }
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Save(text);
+
+}
+
+static void Save(string text)
+{
+    Console.Clear() ;
+    Console.WriteLine("What path to save the file?");
+    var path = Console.ReadLine();
+
+    using (var file = new StreamWriter(path))
+   {
+        file.Write(text);
+   }
+
+    Console.WriteLine("Succed!!");
+    Console.ReadLine();
+    Menu();
+}

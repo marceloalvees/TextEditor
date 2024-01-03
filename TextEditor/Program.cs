408eb65610d5397019ab088adf139b55b10f676e
Marcelo Alves <marcelloalves09@gmail.com>
2024-01-03 19:20:05 -0300

    Adicionando o metodo de abrir arquivo

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -19,7 +19,18 @@ static void Menu()
 
 static void Open()
 {
+    Console.Clear();
+    Console.WriteLine("What is the file path?");
+    string path = Console.ReadLine();
 
+    using(var file = new StreamReader(path))
+    {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+    }
+    Console.WriteLine("");
+    Console.ReadLine();
+    Menu();
 }
 
 static void Closed()
